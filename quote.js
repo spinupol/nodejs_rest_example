243345516d6a88628ff07ddc7b57d0f17f64708c
Sailendra Pinupolu <Spinupolu@gmail.com>
2016-12-05 00:32:36 -0500

    updated default route and all possible routes

--- quote.js
+++ quote.js
@@ -1,62 +1,76 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var app=express();
+var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-var quotes= [
-{ who: 'Trump', what: 'I know words. I have the best words.' },
-{ who: 'Bender', what: 'Bite my shiny metal ass' },
-{ who: 'Teal\'c', what: 'Indeed.' },
-{ who: 'Tyler', what: 'Can I take your picture before I beat you up?' }
+var quotes = [
+    { who: 'Trump', what: 'I know words. I have the best words.' },
+    { who: 'Bender', what: 'Bite my shiny metal ass' },
+    { who: 'Teal\'c', what: 'Indeed.' },
+    { who: 'Tyler', what: 'Can I take your picture before I beat you up?' }
 ];
 
 app.get('/', function(req, res) {
-res.type('text/plain');
-res.send('test REST API');
+    res.type('text/plain');
+    res.write('The available endpoints are\n');
+    res.write('GET /\n');
+    res.write('GET /quote/all\n');
+    res.write('GET /quote/random\n');
+    res.write('GET /quote/:id\n');
+    res.write('POST /quote\n');
+    res.write('DELETE /quote/:id\n');
+    res.end();
 });
 
 app.get('/quote/all', function(req, res) {
-res.json(quotes);
+    res.json(quotes);
 });
 
 app.get('/quote/random', function(req, res) {
-var id = Math.floor(Math.random() * quotes.length);
-var q = quotes[id];
-res.json(q);
+    var id = Math.floor(Math.random() * quotes.length);
+    var q = quotes[id];
+    res.json(q);
 });
 
 app.get('/quote/:id', function(req, res) {
-if(quotes.length <= req.params.id || req.params.id < 0) {
-res.statusCode = 404;
-return res.send('Error 404: index not found');
-}
-var q = quotes[req.params.id];
-res.json(q);
+    if (quotes.length <= req.params.id || req.params.id < 0) {
+        res.statusCode = 404;
+        return res.send('Error 404: index not found');
+    }
+    var q = quotes[req.params.id];
+    res.json(q);
 });
 
 app.post('/quote', urlencodedParser, function(req, res) {
 
-if (!req.body.hasOwnProperty('who') || !req.body.hasOwnProperty('what')) {
-res.statusCode = 400;
-return res.send('Error 400: Post syntax incorrect.');
-}
+    if (!req.body.hasOwnProperty('who') || !req.body.hasOwnProperty('what')) {
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect.');
+    }
 
-var newQuote = {
-who: req.body.who,
-text: req.body.what
-};
+    var newQuote = {
+        who: req.body.who,
+        text: req.body.what
+    };
 
-quotes.push(newQuote);
-res.json(newQuote);
+    quotes.push(newQuote);
+    res.json(newQuote);
 });
 
 app.delete('/quote/:id', function(req, res) {
-if(quotes.length <= req.params.id) {
-res.statusCode = 404;
-return res.send('Error 404: Index not found');
-}
-quotes.splice(req.params.id, 1);
-res.json(true);
+    if (quotes.length <= req.params.id) {
+        res.statusCode = 404;
+        return res.send('Error 404: Index not found');
+    }
+    quotes.splice(req.params.id, 1);
+    res.json(true);
 });
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
+
+var port = 9000;
 app.listen(9000);
+console.log('Listening on port', port);
